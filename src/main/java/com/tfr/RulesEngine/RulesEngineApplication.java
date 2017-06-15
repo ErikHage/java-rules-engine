8676ba99940fc4c9260ecd3af362c78244ee6ee8
Erik Hage <ehage4@gmail.com>
2017-06-14 20:13:36 -0400

    Initialize Project with Spring Intializr

---
+++ src/main/java/com/tfr/RulesEngine/RulesEngineApplication.java
@@ -0,0 +1,12 @@
+package com.tfr.RulesEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RulesEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RulesEngineApplication.class, args);
+	}
+}
