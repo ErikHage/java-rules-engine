0ebdec26d71277fcd2be151da30b5a9f40faef0a
Erik Hage <ehage4@gmail.com>
2017-06-26 20:06:14 -0400

    refactoring to test removing some generic types

---
+++ src/main/java/com/tfr/rulesEngine/exception/DuplicateRuleException.java
@@ -0,0 +1,7 @@
+package com.tfr.rulesEngine.exception;
+
+/**
+ * Created by Erik on 6/26/2017.
+ */
+public class DuplicateRuleException {
+}
---
+++ src/main/java/com/tfr/rulesEngine/rule/GenericRuleSetBuilder.java
@@ -0,0 +1,7 @@
+package com.tfr.rulesEngine.rule;
+
+/**
+ * Created by Erik on 6/26/2017.
+ */
+public class RuleSetBuilder2 {
+}
